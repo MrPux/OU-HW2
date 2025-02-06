44c488a7ba01ea3db81d8bf9646610bacaed5a1d
TONY <os@MacBookPro.lan>
2025-02-06 13:36:05 -0600

    First two constructors finished. (Third constructor is best to do it after methods are completed).

--- StringSet.java
+++ StringSet.java
@@ -1,3 +1,26 @@
 public class StringSet {
+    String[] strings;
+    int size;
+
+    // ---------- Constructors ------------
+
+    // Pre-condition(s): a number greater than zero.
+    public StringSet(int intialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Hey man, your number can't be negative.");
+        }
+
+        this.strings = new Strings[initialCapacity];
+        this.size = 0;
+    }
+
+    // Pre-condition(s): none, initialize the string Array of length ten.
+    public StringSet() {
+        this.strings = new Strings[10];
+        this.size = 0;
+    }
+
+    // Pre-codniton(s): StringSet object. Copy the the object elements into
+    // this.StringSet
 
 }
